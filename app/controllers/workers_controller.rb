0d30a684142f9834951f9bd0474d7a33ea03269b
alander <akhrenov@lanit.ru>
2016-03-27 18:16:08 +0400

    Добавлено поле телефона к сотрудникам

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -229,7 +229,7 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def worker_params
-      params.require(:worker).permit(:fname, :sname, :tname, :birthday, :pin, :isactive)
+      params.require(:worker).permit(:fname, :sname, :tname, :birthday, :pin, :isactive, :phone)
     end
     
       
